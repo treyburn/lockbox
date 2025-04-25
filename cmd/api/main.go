4eb6994273dc086a8c5ce860122634e387010fd7
Travis Reyburn <travisreyburn@gmail.com>
2025-04-25 11:55:57 -0600

    Create and inject a file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -13,10 +13,11 @@ import (
 )
 
 func initializeLogger(cache store.Store) (store.TransactionLog, error) {
-	logger, err := store.NewTransactionLog("transaction.log")
+	file, err := os.OpenFile("transaction.log", os.O_RDWR|os.O_APPEND|os.O_CREATE, 0755)
 	if err != nil {
-		return nil, fmt.Errorf("error creating transaction logger: %w", err)
+		return nil, fmt.Errorf("error opening transaction log file: %w", err)
 	}
+	logger := store.NewTransactionLog(file)
 
 	events, errs := logger.ReadEvents()
 
